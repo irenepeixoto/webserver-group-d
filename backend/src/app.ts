94cbdcf9a9fa0d23feaf65951f3b22ebab152fe4
viniciusym <viniciusyamate5@gmail.com>
2025-01-25 01:08:57 +0000

    feat(fuelPriceService): implement function to get a list of fuel prices filtered by municipality and fuel type

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,10 @@
-import express from "express"; const app = express();
+import express from "express";
+import fuelPriceService from "./services/fuelPriceService";
+const app = express();
 const port = process.env.EXPRESS_PORT || 3000;
 
 app.use(express.json());
 
 app.listen(port, async () => {
   console.log(`listening on port ${port}`);
-  console.log(process.env.SUPABASE_KEY || "sem")
 });
