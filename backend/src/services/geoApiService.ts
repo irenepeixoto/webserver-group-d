1a94288809401e356c756e8f06f63c22e2ad4533
viniciusym <viniciusyamate5@gmail.com>
2025-01-28 17:25:09 +0000

    feat(geoApiService): change getAddressByPostalCode function to return Address model

--- backend/src/services/geoApiService.ts
+++ backend/src/services/geoApiService.ts
@@ -1,10 +1,11 @@
 import BadRequestError from "../errors/BadRequest";
+import Address from "../models/Address";
 import GeoApiResponse from "../types/GeoApiResponse";
 
 const geoApiService = {
   getAddressByPostalCode: async (
     postalCode: string,
-  ): Promise<GeoApiResponse> => {
+  ): Promise<Address> => {
     const postalCodeRegex = /^\d\d\d\d-\d\d\d$/;
     if (!postalCodeRegex.test(postalCode)) {
       throw new BadRequestError("The provided postal code must follow the following format: 1234-567")
@@ -12,7 +13,8 @@ const geoApiService = {
 
     const url = `https://json.geoapi.pt/cp/${postalCode}`;
     const reponse = await fetch(url);
-    const address: GeoApiResponse = await reponse.json();
+    const data: GeoApiResponse = await reponse.json();
+    const address = new Address(data.CP, data.partes[0].Artéria, data.Concelho, data.Distrito);
     return address;
   },
 };
